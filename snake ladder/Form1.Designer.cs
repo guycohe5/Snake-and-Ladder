f0091007ead8eff00fb137a79ed6f565c2859f05
Guy Cohen <guyco27@gmail.com>
2021-11-10 14:22:19 +0200

    snake ladder

---
+++ snake ladder/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace snake_ladder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.board_game = new System.Windows.Forms.Panel();
+            this.Start = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.GameCube = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bluePositionLbl = new System.Windows.Forms.Label();
+            this.redPositionLbl = new System.Windows.Forms.Label();
+            this.displayBlueScore = new System.Windows.Forms.Label();
+            this.displayRedScore = new System.Windows.Forms.Label();
+            this.turnLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // board_game
+            // 
+            this.board_game.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.board_game.Location = new System.Drawing.Point(12, 12);
+            this.board_game.Name = "board_game";
+            this.board_game.Size = new System.Drawing.Size(868, 584);
+            this.board_game.TabIndex = 0;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(12, 611);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(143, 55);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.textBox1.Font = new System.Drawing.Font("MV Boli", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(1070, 223);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(115, 115);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // GameCube
+            // 
+            this.GameCube.Enabled = false;
+            this.GameCube.Location = new System.Drawing.Point(1070, 357);
+            this.GameCube.Name = "GameCube";
+            this.GameCube.Size = new System.Drawing.Size(115, 47);
+            this.GameCube.TabIndex = 2;
+            this.GameCube.Text = "Roll";
+            this.GameCube.UseVisualStyleBackColor = true;
+            this.GameCube.Click += new System.EventHandler(this.GameCube_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(905, 496);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(110, 86);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::snake_ladder.Properties.Resources.soldier_2;
+            this.pictureBox2.Location = new System.Drawing.Point(59, 20);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(53, 66);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::snake_ladder.Properties.Resources.soldier_1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 66);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bluePositionLbl
+            // 
+            this.bluePositionLbl.AutoSize = true;
+            this.bluePositionLbl.Location = new System.Drawing.Point(1058, 35);
+            this.bluePositionLbl.Name = "bluePositionLbl";
+            this.bluePositionLbl.Size = new System.Drawing.Size(18, 20);
+            this.bluePositionLbl.TabIndex = 4;
+            this.bluePositionLbl.Text = "0";
+            // 
+            // redPositionLbl
+            // 
+            this.redPositionLbl.AutoSize = true;
+            this.redPositionLbl.Location = new System.Drawing.Point(1058, 76);
+            this.redPositionLbl.Name = "redPositionLbl";
+            this.redPositionLbl.Size = new System.Drawing.Size(18, 20);
+            this.redPositionLbl.TabIndex = 5;
+            this.redPositionLbl.Text = "0";
+            // 
+            // displayBlueScore
+            // 
+            this.displayBlueScore.AutoSize = true;
+            this.displayBlueScore.Location = new System.Drawing.Point(965, 35);
+            this.displayBlueScore.Name = "displayBlueScore";
+            this.displayBlueScore.Size = new System.Drawing.Size(91, 20);
+            this.displayBlueScore.TabIndex = 4;
+            this.displayBlueScore.Text = "Blue Score:";
+            // 
+            // displayRedScore
+            // 
+            this.displayRedScore.AutoSize = true;
+            this.displayRedScore.Location = new System.Drawing.Point(967, 76);
+            this.displayRedScore.Name = "displayRedScore";
+            this.displayRedScore.Size = new System.Drawing.Size(89, 20);
+            this.displayRedScore.TabIndex = 5;
+            this.displayRedScore.Text = "Red Score:";
+            // 
+            // turnLbl
+            // 
+            this.turnLbl.AutoSize = true;
+            this.turnLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.turnLbl.Location = new System.Drawing.Point(957, 114);
+            this.turnLbl.Name = "turnLbl";
+            this.turnLbl.Size = new System.Drawing.Size(175, 37);
+            this.turnLbl.TabIndex = 6;
+            this.turnLbl.Text = "Turn: Blue";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(921, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "אדום זה נחש ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(921, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "לבן כחול זה סולם";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1197, 718);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.turnLbl);
+            this.Controls.Add(this.displayRedScore);
+            this.Controls.Add(this.displayBlueScore);
+            this.Controls.Add(this.redPositionLbl);
+            this.Controls.Add(this.bluePositionLbl);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.GameCube);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.board_game);
+            this.Name = "Form1";
+            this.Text = "snake ladder";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel board_game;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button GameCube;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label bluePositionLbl;
+        private System.Windows.Forms.Label redPositionLbl;
+        private System.Windows.Forms.Label displayBlueScore;
+        private System.Windows.Forms.Label displayRedScore;
+        private System.Windows.Forms.Label turnLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ snake ladder/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snake_ladder
+{
+    public partial class Form1 : Form
+    {
+        Button[] buttons = null;
+        Random random = new Random();
+        int Lines;
+        bool turn = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        //בניית לוח המשחק 
+
+        public void initBoard()
+        {
+            
+            board_game.Controls.Clear();
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(pictureBox2);
+
+            Graphics gr = board_game.CreateGraphics();
+            Pen MyPen = new Pen(Brushes.Black, 4);
+
+            int Size_board = random.Next(4, 10);
+            //int Size_board = 4;
+            this.buttons = new Button[Size_board * Size_board];
+
+            pictureBox1.Tag = 0;
+            pictureBox2.Tag = 0;
+
+            Lines = Convert.ToInt32(Size_board);
+
+            //int Lines = Convert.ToInt32(10);
+            float x = 0f;
+            float y = 0f;
+            float X_Space = ((board_game.Width - MyPen.Width) * 1.0f / Lines);
+            float Y_Space = ((board_game.Height - MyPen.Width) * 1.0f / Lines);
+            Font MyFont = new Font("Arial", (board_game.Width <= board_game.Height)
+                ? X_Space / 3 : Y_Space / 3);
+
+            for (int i = 0; i < Lines; i++)
+            {
+                gr.DrawLine(MyPen, x, y, x, Y_Space * Lines);
+                x += X_Space;
+            }
+
+
+            x = 0f;
+
+            for (int i = 0; i < Lines; i++)
+            {
+                gr.DrawLine(MyPen, x, y, X_Space * Lines, y);
+                y += Y_Space;
+            }
+
+            x = 0f;
+            y = 0f;
+            int Counter = Lines * Lines - 1;
+
+            int top = 0, left = 0;
+            int golds = 2;
+
+            for (int text_i = Lines -1; text_i >= 0 ; text_i--)
+            {
+                for (int text_j = Lines -1 ; text_j >= 0 ; text_j--)
+                {
+                    Button l = new Button();
+                    l.Width = (int)X_Space;
+                    l.Height = (int)Y_Space;
+                    l.Text = Counter.ToString();
+                    l.Top = top;
+                    l.Left = left;
+                    l.FlatStyle = FlatStyle.Flat;
+                    
+                    
+                    if (Counter < Lines * Lines - 1)
+                    {
+                        if (random.Next(3) == 1)
+                        {
+                            int randomIndex = Counter;
+
+                            int midLength = (Lines * Lines - 1) / 2;
+
+                            if (golds > 0 && randomIndex < midLength)
+                            {
+                                l.Tag = -1;
+                                golds--;
+                                l.BackColor = Color.Gold;
+                            }
+                            else
+                            {
+
+
+                                while (randomIndex == Counter)
+                                {
+                                    randomIndex = random.Next(Size_board * Size_board - 1);
+                                }
+
+                                if (randomIndex < Counter)
+                                    l.BackColor = Color.Red;
+                                else
+                                    l.BackColor = Color.AliceBlue;
+
+                                l.Tag = randomIndex;
+                            }
+
+                        }
+                    }
+                   
+
+
+
+                    buttons[Counter] = l;
+
+
+                    board_game.Controls.Add(l);
+
+
+                    x += X_Space;
+                    left += (int)X_Space;
+
+                    Counter--;
+                }
+                y += Y_Space;
+                top += (int)Y_Space;
+                left = 0;
+                x = 0f;
+
+            }
+        }
+
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            GameCube.Enabled = true;
+            initBoard();
+
+        }
+
+        private Button getCell(int value)
+        {
+            foreach (Button button in buttons)
+            {
+                if (button.Text == value.ToString())
+                {
+                    return button;
+                }
+            }
+            return null;
+        }
+
+        private void clearBorders()
+        {
+            foreach (Button button in buttons)
+            {
+                button.FlatAppearance.BorderColor = Color.Black;
+                button.FlatAppearance.BorderSize = 1;
+            }
+        }
+        bool isGloded = false;
+        private void updateSolider(PictureBox pictureBox, int Roll)
+        {
+            clearBorders();
+
+
+            bluePositionLbl.Text = ((int)(pictureBox1.Tag)).ToString() + " - ";
+            redPositionLbl.Text = ((int)(pictureBox2.Tag)).ToString() + " - ";
+
+
+            Button prevCell = getCell((int)(pictureBox.Tag));
+            prevCell.FlatAppearance.BorderColor = Color.Green;
+            prevCell.FlatAppearance.BorderSize = 3;
+
+            pictureBox.Tag = (int)(pictureBox.Tag) + Roll;
+
+            if ((int)(pictureBox.Tag) + Roll < Lines * Lines)
+            {
+                Button nextCell = getCell((int)(pictureBox.Tag));
+                if (nextCell != null)
+                {
+                    nextCell.FlatAppearance.BorderColor = Color.Green;
+                    nextCell.FlatAppearance.BorderSize = 3;
+                }
+
+
+
+
+                prevCell.Controls.Remove(pictureBox);
+                nextCell.Controls.Add(pictureBox);
+
+                if (nextCell.Tag != null)
+                {
+
+                    if ((int)nextCell.Tag == -1)
+                    {
+
+                        
+
+                        int bluePostition = (int)pictureBox1.Tag;
+                        int redPostition = (int)pictureBox2.Tag;
+
+                        if ((turn && bluePostition < redPostition) || (!turn && bluePostition > redPostition))
+                        {
+                            isGloded = true;
+                            Button blueCell = getCell(bluePostition);
+                            Button redCell = getCell(redPostition);
+
+                            pictureBox1.Tag = redPostition;
+                            pictureBox2.Tag = bluePostition;
+
+
+                            blueCell.Controls.Clear();
+                            blueCell.Controls.Add(pictureBox2);
+
+                            redCell.Controls.Clear();
+                            redCell.Controls.Add(pictureBox1);
+                        }
+                    }
+                    else
+                    {
+                        Button targetCell = getCell((int)nextCell.Tag);
+                        pictureBox.Tag = int.Parse(targetCell.Text);
+                        prevCell.Controls.Remove(pictureBox);
+                        targetCell.Controls.Add(pictureBox);
+                    }
+
+
+                }
+            }
+
+            else
+            {
+                MessageBox.Show(turn ? "הכחול ניצח!" : "האדום ניצח");
+                initBoard();
+            }
+
+            
+
+
+            //foreach (Button button in buttons)
+            //{
+            //    if (int.Parse(button.Text) == (int)(pictureBox.Tag))
+            //    {
+
+            //        if (button.Tag != null)
+            //        {
+            //            if ((int)(button.Tag) == -1)
+            //            {
+            //                int bluePostition = (int)pictureBox1.Tag;
+            //                int redPostition = (int)pictureBox2.Tag;
+            //                pictureBox1.Tag = redPostition;
+            //                pictureBox2.Tag = bluePostition;
+
+            //                if ((turn && bluePostition < redPostition) || (!turn && bluePostition > redPostition))
+            //                {
+
+            //                    Button blueLabel = getCell(bluePostition);
+            //                    Button redLabel = getCell(redPostition);
+
+            //                    blueLabel.Controls.Remove(pictureBox1);
+            //                    redLabel.Controls.Add(pictureBox2);
+
+            //                    break;
+            //                }
+            //                else
+            //                    button.Controls.Add(pictureBox);
+            //            }
+            //            else
+            //            {
+            //                int updatedPosition = (int)(button.Tag);
+            //                pictureBox.Tag = updatedPosition;
+            //                foreach (Button button1 in buttons)
+            //                {
+            //                    if (int.Parse(button1.Text) == updatedPosition)
+            //                    {
+            //                        button1.Controls.Add(pictureBox);
+            //                    }
+            //                }
+            //            }
+            //        }
+            //        else
+            //        {
+            //            button.Controls.Add(pictureBox);
+            //        }
+
+                    
+            //    }
+            //}
+
+
+            
+
+
+            bluePositionLbl.Text += ((int)(pictureBox1.Tag)).ToString();
+            redPositionLbl.Text += ((int)(pictureBox2.Tag)).ToString();
+
+        }
+
+        private void GameCube_Click(object sender, EventArgs e)
+        {
+
+            Random R_Roll = new Random();
+            int Roll = R_Roll.Next(1, Lines);
+            textBox1.Text = Roll.ToString();
+            updateSolider(turn ? pictureBox1 : pictureBox2, Roll);
+            turn = !turn;
+            if (isGloded)
+            {
+                turnLbl.BackColor = Color.Gold;
+                turnLbl.Text = turn ? "Turn: Blue" : "Turn: Red";
+                isGloded = false;
+            }
+            else
+            {
+                turnLbl.Text = turn ? "Turn: Blue" : "Turn: Red";
+                turnLbl.BackColor = Color.Transparent;
+            }
+        }
+    }
+}
